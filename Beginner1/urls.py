ed921e7a52cf4edd6f9c4624def2db846c8e2553
Aayushman Ojha <abhishekojha.edu@gmail.com>
2025-09-20 19:09:56 +0530

    Added Note App With CRUD Functionality

--- Beginner1/urls.py
+++ Beginner1/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('pages.urls')),
+    path('pages/', include('pages.urls')),
+    path('notes/', include('notes.urls')),
 ]
